c9f71e758a4fcebb6e8381fdbd063ad303772d18
Arpanghorai96 <121602804+Arpanghorai96@users.noreply.github.com>
2023-12-26 21:59:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let year=parseInt(prompt("Eenter a Year "))
+let daysOfAYear=(year)=>{
+	if(((year%4==0)&&(year%100!=0))||(y%400==0))
+		return 366;
+	else
+		return 365;
+}
+daysOfAYear(year);
